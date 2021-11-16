e91b69ac0a24d74844625bf152a931c9d09a786d
Chase <chasestruse@gmail.com>
2021-11-16 06:34:36 -0600

    Added header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title: string = 'To Do List';
+
   constructor() { }
 
   ngOnInit(): void {
